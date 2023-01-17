cc64ca76c92f398bccfb9b2fab9cb19879620343
sandeep pratihari <spratihari1@gmail.com>
2023-01-17 22:03:30 +0530

    sol 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,6 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
 
-    DeluxePizza dp= new DeluxePizza(true);
-    System.out.println(dp.getBill());
-    System.out.println("-------------------------------");
-
-    DeluxePizza dp1= new DeluxePizza(false);
-    System.out.println(dp1.getBill());
 
 
   }
