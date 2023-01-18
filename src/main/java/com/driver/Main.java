0ffa7e66c8cd1e04011c775eea2be834b61df0d6
sandeep pratihari <spratihari1@gmail.com>
2023-01-18 12:08:53 +0530

    sol 11

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,14 +3,14 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
 
-    DeluxePizza p = new DeluxePizza(false);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+//    DeluxePizza p = new DeluxePizza(false);
+//    p.addExtraCheese();
+//    p.addExtraToppings();
+//    p.addExtraCheese();
+//    p.addExtraToppings();
+//    p.addTakeaway();
+//    p.addTakeaway();
+//    System.out.println(p.getBill());
 
 
 
